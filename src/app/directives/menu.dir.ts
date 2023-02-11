213db394e98554bc7a532feba64ddca4bf7fdc3b
alansealy0914 <alan.sealy@gmail.com>
2023-02-11 14:31:35 -0600

    1 file modified

--- src/app/directives/menu.dir.ts
+++ src/app/directives/menu.dir.ts
@@ -5,7 +5,7 @@ import { CartService } from '../services/cart.service';
   template : `
   <nav class="navbar navbar-expand-lg navbar-light bg-light d-print-none">
 
-  <a class="navbar-brand" href="#" routerLink = '/products'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROSGhNugPi-wkkvEePwHNppt8AaZRlUk-y9yEapagLNUaXxy_G" class="logo mr-2"/>Pop that <em>"ish"</em> PopCorn Ki<a class="navbar-brand" href="#" routerLink = '/products'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROSGhNugPi-wkkvEePwHNppt8AaZRlUk-y9yEapagLNUaXxy_G" class="logo mr-2"/>Pop <em>that ish</em> Shopping</a>
+  <a class="navbar-brand" href="#" routerLink = '/products'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROSGhNugPi-wkkvEePwHNppt8AaZRlUk-y9yEapagLNUaXxy_G" class="logo mr-2"/>Pop that <em>"ish"</em> PopCorn Kitchen<a class="navbar-brand" href="#" routerLink = '/products'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROSGhNugPi-wkkvEePwHNppt8AaZRlUk-y9yEapagLNUaXxy_G" class="logo mr-2"/></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
