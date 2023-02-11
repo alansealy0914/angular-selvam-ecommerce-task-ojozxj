74bd2ed02608f89e32085e83d6240a2b98d9fbf4
alansealy0914 <alan.sealy@gmail.com>
2023-02-10 18:10:54 -0600

    1 file modified

--- src/app/directives/menu.dir.ts
+++ src/app/directives/menu.dir.ts
@@ -4,7 +4,7 @@ import { CartService } from '../services/cart.service';
   selector : 'menu',
   template : `
   <nav class="navbar navbar-expand-lg navbar-light bg-light d-print-none">
-  <a class="navbar-brand" href="#" routerLink = '/products'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROSGhNugPi-wkkvEePwHNppt8AaZRlUk-y9yEapagLNUaXxy_G" class="logo mr-2"/>Trend Shopping</a>
+  <a class="navbar-brand" href="#" routerLink = '/products'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROSGhNugPi-wkkvEePwHNppt8AaZRlUk-y9yEapagLNUaXxy_G" class="logo mr-2"/>Pop <em>that ish</em> Shopping</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
